e7d753ac33b482c1a102cf62973f16d7d602cc1b
Nicholas Tierney <nicholas.tierney@gmail.com>
2016-10-27 12:55:21 +1000

    add `...` to arguments, as per #7

--- R/neighbourhoods.R
+++ R/neighbourhoods.R
@@ -5,7 +5,7 @@
 #' This call retuns the list of neighbourhoods for a force, https://data.police.uk/docs/method/neighbourhoods/
 #'
 #' @param neighbourhood a text string of a neighbourhood in the UK
-#'
+#' @param ... further arguments passed to or from other methods. For example, verbose option can be added with ukp_api("call", config = httr::verbose())
 #' @return tibble with columns id and name. id is a Police force specific team identifier, note that this identifier is not unique and may also be used by a different force. Name is the name for the neighbourhood
 #' @export
 #'
@@ -27,7 +27,7 @@
 #
 #' }
 #'
-ukp_neighbourhood <- function(neighbourhood){
+ukp_neighbourhood <- function(neighbourhood, ...){
 
   result <- ukp_api(sprintf("api/%s/neighbourhoods",neighbourhood))
 
@@ -38,13 +38,13 @@ ukp_neighbourhood <- function(neighbourhood){
 #'
 #' \code{ukp_neighbourhood_team} takes a longitude/latitude string and finds the associated neighbourhood code associated with that. Read more here https://data.police.uk/docs/method/neighbourhood-locate/
 #'
-#' @param lat_long a character string of
-#'
+#' @param lat_long a character string of latitude and longitude
+#' @param ... further arguments passed to or from other methods. For example, verbose option can be added with ukp_api("call", config = httr::verbose())
 #' @return a tibble
 #'
 #' @export
 #'
-ukp_neighbourhood_team <- function(lat_long){
+ukp_neighbourhood_team <- function(lat_long, ...){
   # lat_long = c("51.500617,-0.124629")
   result <- ukp_api(sprintf("api/locate-neighbourhood?q=%s", lat_long))
 
