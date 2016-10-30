d0762565036e372cb365a74e77d74d682ec5f65b
Nicholas Tierney <nicholas.tierney@gmail.com>
2016-10-30 11:40:42 +1000

    add outline for crime api

---
+++ R/crime.R
@@ -0,0 +1,17 @@
+# crime-related ----------------------------------------------------------------
+
+# Crimes at street-level; either within a 1 mile radius of a single point, or within a custom area.
+#
+# The street-level crimes returned in the API are only an approximation of where the actual crimes occurred, they are not the exact locations. See the about page for more information about location anonymisation.
+#
+# If you're allowing users to search for locations in Scotland, please make it clear that, since only the British Transport Police provide data for Scotland, crime levels may appear much lower than they really are.
+
+
+
+# Street level crimes
+# Street level outcomes
+# Crimes at location
+# Crimes with no location
+# Crime categories
+
+# Outcomes for a specific crime
