1a9feee4ec65597a0acb6d2f5b79afa166562df3
Nicholas Tierney <nicholas.tierney@gmail.com>
2016-10-30 11:40:53 +1000

    add scaffolding for forces

---
+++ R/forces.R
@@ -0,0 +1,56 @@
+#' forces
+#'
+#' Forces
+#'
+#' A list of all the police forces available via the API. Unique force identifiers obtained here are used in requests for force-specific data via other methods.
+#'
+#' ukp_forces
+#'
+#' List of forces
+#' @return table with columns id, and name. id is a	Unique force identifier, name is a	Force name.
+#'
+#'
+#' Specific force
+#'
+#' ukp_forces_specific
+#'
+#' https://data.police.uk/docs/method/force/
+#' returns this information
+    # Tag	Description
+    # description	Description
+    # url	Force website URL
+    # engagement-methods	Ways to keep informed
+    # url	Method website URL
+    # description	Method description
+    # title	Method title
+    # telephone	Force telephone number
+    # id	Unique force identifier
+    # name	Force name
+#'
+#' Force senior officers
+#'
+#' ukp_forces_people
+#'
+#' returns the following information:
+    # Tag	Description
+    # bio	Senior officer biography (if available)
+    # contact_details	Contact details for the senior officer
+    # email	Email address
+    # telephone	Telephone number
+    # mobile	Mobile number
+    # fax	Fax number
+    # web	Website address
+    # address	Street address
+    # facebook	Facebook profile URL
+    # twitter	Twitter profile URL
+    # youtube	YouTube profile URL
+    # myspace	Myspace profile URL
+    # bebo	Bebo profile URL
+    # flickr	Flickr profile URL
+    # google-plus	Google+ profile URL
+    # forum	Forum URL
+    # e-messaging	E-messaging URL
+    # blog	Blog URL
+    # rss	RSS URL
+    # name	Name of the person
+    # rank	Force rank
